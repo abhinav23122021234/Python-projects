a6bd9bf961e22cde20749d321b36ed48d9e38ef1
abhinav23122021234 <abhi.sharma19jan@gmail.com>
2025-02-16 21:15:28 +0530

    got squirrel counts from the data and made new csv file of it

--- working with csv_files/data_analysis_squirrel_census.py
+++ working with csv_files/data_analysis_squirrel_census.py
@@ -1,1 +1,13 @@
-import pandas
+import pandas as pd
+df=pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+print(df.head())
+grey_squirrel_count=len(df[df["Primary Fur Color"]=="Gray"])
+red_squirrel_count=len(df[df["Primary Fur Color"]=="Cinnamon"])
+Black_squirrel_count=len(df[df["Primary Fur Color"]=="Black"])
+data_dict={
+    "Fur Color": ["Gray","Red","Black"] ,
+    "Squirrel Count": [grey_squirrel_count,red_squirrel_count,Black_squirrel_count]
+}
+new_data=pd.DataFrame(data_dict)
+print(new_data)
+new_data.to_csv("squirrel_count.csv")
